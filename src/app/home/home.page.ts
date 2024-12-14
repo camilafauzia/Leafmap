b9048426e04e1bf3867414f9b687c6292734a101
camilafauzia <camilafauzia@mail.ugm.ac.id>
2024-12-15 00:00:28 +0700

    tambah data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,58 +10,51 @@ export class HomePage {
 
   map!: L.Map;
 
+  // Data mahasiswa
+  dataMahasiswa = [
+    {
+      "no": 1,
+      "first_name": "Alia",
+      "last_name": "Maulana Resti",
+      "latitude": -7.8667599,
+      "longitude": 110.3214134
+    },
+    {
+      "no": 2,
+      "first_name": "ALYA",
+      "last_name": "SYAHIDHA",
+      "latitude": -7.7867386,
+      "longitude": 110.5948413
+    },
+    // Tambahkan data lainnya dari file JSON...
+  ];
+
   constructor() {}
 
   ngOnInit() {}
 
   ionViewDidEnter() {
-    // Koordinat di Indonesia (misalnya di Jakarta)
-    this.map = L.map('mapId').setView([-6.2088, 106.8456], 13);
+    // Inisialisasi peta
+    this.map = L.map('mapId').setView([-7.797068, 110.370529], 10);
 
-    // Opsi peta basemap
+    // Tambahkan layer basemap
     const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
-
-    // const satellite = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    //   attribution: 'Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap (CC-BY-SA)'
-    // });
-
-    // const watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg', {
-    //   attribution: '&copy; <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>'
-    // });
-
-    const terrain = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data: &copy; OpenStreetMap contributors'
-    });
-
-    const cartoLight = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    });
-
-    // Tambahkan salah satu layer basemap ke peta (default OSM)
     osm.addTo(this.map);
 
-    // Buat custom marker menggunakan Font Awesome
-    const customIcon = L.divIcon({
-      html: '<i class="fa-solid fa-location-dot" style="font-size: 24px; color: red;"></i>',
-      className: 'custom-div-icon',
-      iconSize: [24, 24],
-      iconAnchor: [12, 24],
+    // Tambahkan marker untuk setiap mahasiswa
+    this.dataMahasiswa.forEach(mahasiswa => {
+      const customIcon = L.divIcon({
+        html: '<i class="fa-solid fa-location-dot" style="font-size: 24px; color: red;"></i>',
+        className: 'custom-div-icon',
+        iconSize: [24, 24],
+        iconAnchor: [12, 24],
+      });
+
+      const marker = L.marker([mahasiswa.latitude, mahasiswa.longitude], { icon: customIcon })
+        .addTo(this.map)
+        .bindPopup(`<b>${mahasiswa.first_name} ${mahasiswa.last_name}</b>`);
     });
-
-    // Tambahkan marker dengan ikon kustom Font Awesome
-    const marker = L.marker([-6.2088, 106.8456], { icon: customIcon }).addTo(this.map);
-    marker.bindPopup('Jakarta').openPopup();
-
-    // Kontrol layer untuk basemap
-    const baseMaps = {
-      "OpenStreetMap": osm,
-      "Terrain": terrain,
-      "Carto Light": cartoLight
-    };
-
-    // Menambahkan kontrol layer ke peta
-    L.control.layers(baseMaps).addTo(this.map);
   }
 }
